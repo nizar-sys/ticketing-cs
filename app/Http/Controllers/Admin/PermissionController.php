a99ae1818f035ae66515b891617244f5e4c98fce
nizar-sys <raspbery132@gmail.com>
2024-01-29 11:31:19 +0700

    feat: crud permission to repository pattern

--- app/Http/Controllers/Admin/PermissionController.php
+++ app/Http/Controllers/Admin/PermissionController.php
@@ -5,11 +5,18 @@ namespace App\Http\Controllers\Admin;
 use App\DataTables\Admin\PermissionsDataTable;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RequestPermission;
+use App\Services\Permission\PermissionService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Spatie\Permission\Models\Permission;
 
 class PermissionController extends Controller
 {
+    public function __construct(protected PermissionService $permissionService)
+    {
+    }
+
     public function index(PermissionsDataTable $dataTable)
     {
         return $dataTable->render('admin.master_data.permissions.index');
@@ -22,7 +29,14 @@ class PermissionController extends Controller
 
     public function store(RequestPermission $request)
     {
-        Permission::create($request->validated());
+        try {
+            $this->permissionService->create($request->validated());
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            Log::error($e->getMessage());
+            return redirect()->route('permission.index')->with('error', $e->getMessage());
+        }
 
         return redirect()->route('permission.index')->with('success', 'Permission created successfully!');
     }
@@ -34,14 +48,29 @@ class PermissionController extends Controller
 
     public function update(RequestPermission $request, Permission $permission)
     {
-        $permission->update($request->validated());
+        try {
+            $this->permissionService->update($permission->id, $request->validated());
+        } catch (\Exception $e) {
+            DB::rollBack();
+            Log::error($e->getMessage());
+            return redirect()->route('permission.index')->with('error', $e->getMessage());
+        }
 
         return redirect()->route('permission.index')->with('success', 'Permission updated successfully!');
     }
 
     public function destroy(Permission $permission)
     {
-        $permission->delete();
+        try {
+            $this->permissionService->delete($permission->id);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            Log::error($e->getMessage());
+            return response()->json([
+                'status' => 'error',
+                'message' => $e->getMessage()
+            ]);
+        }
 
         return response()->json([
             'status' => 'success',
